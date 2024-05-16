371cc2b6e8283ffc76cbc1463d62fc4d4a883c50
Prajakta Darekar <114302180+prajudarekar04@users.noreply.github.com>
2024-05-16 18:32:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('QuickSumm.html')
+    return render_template('index.html')
 
 @app.route('/summarize', methods=['POST'])
 def summarization():
     if request.method == 'POST':
         text = request.form['input_text']
         summary = summarize_text(text)
-        return render_template('QuickSumm.html', input_text=text, summary=summary,text=text)
+        return render_template('index.html', input_text=text, summary=summary,text=text)
 
 if __name__ == '__main__':
     app.run(debug=True)
